dfaa932c7dc8aafd54c1ee3aae18b12c694879f4
Mark Wallace <lotabout@gmail.com>
2015-12-26 18:34:57 +0800

    [step 0] the framework
    read the source file and output every single character as token

---
+++ xc-tutor.c
@@ -0,0 +1,67 @@
+// this file is used for tutorial to build the compiler step by step
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+int token;            // current token
+char *src, *old_src;  // pointer to source code string;
+int poolsize;         // default size of text/data/stack
+int line;             // line number
+
+void next() {
+    token = *src++;
+    return;
+}
+
+void expression(int level) {
+    // do nothing
+}
+
+void program() {
+    next(); // get next token
+    while (token > 0) {
+        printf("token is: %c\n", token);
+        next();
+    }
+}
+
+
+int eval() {
+    // do nothing yet
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+
+    int i, fd;
+
+    argc--;
+    argv++;
+
+    poolsize = 256 * 1024; // arbitrary size
+    line = 1;
+
+    if ((fd = open(*argv, 0)) < 0) {
+        printf("could not open(%s)\n", *argv);
+        return -1;
+    }
+
+    if (!(src = old_src = malloc(poolsize))) {
+        printf("could not malloc(%d) for source area\n", poolsize);
+        return -1;
+    }
+    // read the source file
+    if ((i = read(fd, src, poolsize-1)) <= 0) {
+        printf("read() returned %d\n", i);
+        return -1;
+    }
+    src[i] = 0; // add EOF character
+    close(fd);
+
+    program();
+    return eval();
+}
