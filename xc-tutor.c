3840ed753e2e8102919d522bbd9ec002d162f825
Mark Wallace <lotabout@gmail.com>
2015-12-28 08:33:05 +0800

    [step-1] build a virtual machine
    initialize all variables.

--- xc-tutor.c
+++ xc-tutor.c
@@ -10,6 +10,16 @@ int token;            // current token
 char *src, *old_src;  // pointer to source code string;
 int poolsize;         // default size of text/data/stack
 int line;             // line number
+int *text,            // text segment
+    *old_text,        // for dump text segment
+    *stack;           // stack
+char *data;           // data segment
+int *pc, *bp, *sp, ax, cycle; // virtual machine registers
+
+// instructions
+enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
+       OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
+       OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
 
 void next() {
     token = *src++;
@@ -62,6 +72,27 @@ int main(int argc, char **argv)
     src[i] = 0; // add EOF character
     close(fd);
 
+    // allocate memory for virtual machine
+    if (!(text = old_text = malloc(poolsize))) {
+        printf("could not malloc(%d) for text area\n", poolsize);
+        return -1;
+    }
+    if (!(data = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
+    if (!(stack = malloc(poolsize))) {
+        printf("could not malloc(%d) for stack area\n", poolsize);
+        return -1;
+    }
+
+    memset(text, 0, poolsize);
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
+    bp = sp = (int *)((int)stack + poolsize);
+    ax = 0;
+
     program();
+
     return eval();
 }
